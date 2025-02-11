b201e9ba167173f6893ba7436818b5b57632f765
XtraDavido <davidokemink8@gmail.com>
2025-02-11 19:36:28 +0100

    Attempt at fixing Item/Block id issues

--- src/main/java/net/xtradavido/actualhay/block/ModBlocks.java
+++ src/main/java/net/xtradavido/actualhay/block/ModBlocks.java
@@ -12,23 +12,25 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.xtradavido.actualhay.ActualHay;
 
 public class ModBlocks {
     public static final Block STRAW_BLOCK = registerBlock("straw_block",
-            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).instrument(NoteBlockInstrument.BANJO).strength(0.5F).sounds(BlockSoundGroup.GRASS)));
+            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).instrument(NoteBlockInstrument.BANJO).strength(0.5F).sounds(BlockSoundGroup.GRASS).registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ActualHay.MOD_ID, "straw_block")))));
     public static final Block HAY_CARPET = registerBlock("hay_carpet",
-            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).strength(0.5F).sounds(BlockSoundGroup.GRASS).pistonBehavior(PistonBehavior.DESTROY)));
+            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).strength(0.5F).sounds(BlockSoundGroup.GRASS).pistonBehavior(PistonBehavior.DESTROY).registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ActualHay.MOD_ID, "hay_carpet")))));
     public static final Block STRAW_CARPET = registerBlock("straw_carpet",
-            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).strength(0.5F).sounds(BlockSoundGroup.GRASS).pistonBehavior(PistonBehavior.DESTROY)));
+            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).strength(0.5F).sounds(BlockSoundGroup.GRASS).pistonBehavior(PistonBehavior.DESTROY).registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ActualHay.MOD_ID, "straw_carpet")))));
     public static final Block THATCH_BLOCK = registerBlock("thatch_block",
-            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).strength(0.5F).sounds(BlockSoundGroup.GRASS)));
+            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).strength(0.5F).sounds(BlockSoundGroup.GRASS).registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ActualHay.MOD_ID, "thatch_block")))));
     public static final Block THATCH_SLAB = registerBlock("thatch_slab",
-            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).strength(0.5F).sounds(BlockSoundGroup.GRASS)));
+            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).strength(0.5F).sounds(BlockSoundGroup.GRASS).registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ActualHay.MOD_ID, "thatch_slab")))));
     public static final Block THATCH_STAIRS = registerBlock("thatch_stairs",
-            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).strength(0.5F).sounds(BlockSoundGroup.GRASS)));
+            new Block(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).strength(0.5F).sounds(BlockSoundGroup.GRASS).registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(ActualHay.MOD_ID, "thatch_stairs")))));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
--- src/main/java/net/xtradavido/actualhay/item/ModItems.java
+++ src/main/java/net/xtradavido/actualhay/item/ModItems.java
@@ -6,15 +6,17 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.xtradavido.actualhay.ActualHay;
 
 public class ModItems {
-    public static final Item GRASS_ITEM = registerItem("grass", new Item(new Item.Settings()));
-    public static final Item DRIED_GRASS_ITEM = registerItem("dried_grass", new Item(new Item.Settings()));
-    public static final Item HAY = registerItem("hay", new Item(new Item.Settings()));
-    public static final Item GRAIN = registerItem("grain", new Item(new Item.Settings()));
-    public static final Item STRAW = registerItem("straw", new Item(new Item.Settings()));
+    public static final Item GRASS_ITEM = registerItem("grass", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ActualHay.MOD_ID, "grass")))));
+    public static final Item DRIED_GRASS_ITEM = registerItem("dried_grass", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ActualHay.MOD_ID, "dried_grass")))));
+    public static final Item HAY = registerItem("hay", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ActualHay.MOD_ID, "hay")))));
+    public static final Item GRAIN = registerItem("grain", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ActualHay.MOD_ID, "grain")))));
+    public static final Item STRAW = registerItem("straw", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ActualHay.MOD_ID, "straw")))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(ActualHay.MOD_ID, name), item);
