0f2b25d3130c303c9be091b2092b8c907c95c93b
XtraDavido <davidokemink8@gmail.com>
2025-02-13 00:59:12 +0100

    Added Data Generation

--- src/main/java/net/xtradavido/actualhay/datagen/ModItemTagProvider.java
+++ src/main/java/net/xtradavido/actualhay/datagen/ModItemTagProvider.java
@@ -3,13 +3,12 @@ package net.xtradavido.actualhay.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
 
 public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
-    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture, @Nullable BlockTagProvider blockTagProvider) {
-        super(output, completableFuture, blockTagProvider);
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
     }
 
     @Override
