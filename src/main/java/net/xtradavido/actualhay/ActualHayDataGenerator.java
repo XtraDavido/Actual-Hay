0c917844e7c5d9647dded8c4ab534f16f4a25f5a
XtraDavido <davidokemink8@gmail.com>
2025-02-13 00:58:29 +0100

    Added Data Generation

--- src/main/java/net/xtradavido/actualhay/ActualHayDataGenerator.java
+++ src/main/java/net/xtradavido/actualhay/ActualHayDataGenerator.java
@@ -2,10 +2,19 @@ package net.xtradavido.actualhay;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.xtradavido.actualhay.datagen.ModBlockTagProvider;
+import net.xtradavido.actualhay.datagen.ModItemTagProvider;
+import net.xtradavido.actualhay.datagen.ModLootTableProvider;
+import net.xtradavido.actualhay.datagen.ModModelProvider;
 
 public class ActualHayDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
 	}
 }
