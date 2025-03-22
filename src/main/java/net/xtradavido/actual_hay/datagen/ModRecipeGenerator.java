962e0033991455b94cce2956fac03aa59d64bcfd
XtraDavido <davidokemink8@gmail.com>
2025-03-22 11:42:48 +0100

    Added ModRecipeGenerator (unused)

---
+++ src/main/java/net/xtradavido/actual_hay/datagen/ModRecipeGenerator.java
@@ -0,0 +1,25 @@
+package net.xtradavido.actual_hay.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.recipe.RecipeExporter;
+import net.minecraft.data.recipe.RecipeGenerator;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeGenerator extends FabricRecipeProvider {
+    public ModRecipeGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup registryLookup, RecipeExporter exporter) {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
